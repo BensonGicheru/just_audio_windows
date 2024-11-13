64c48a308bf3e121fc377b8150b5a7bca0c46316
BenGicheru <bensgich@gmail.com>
2024-11-13 19:04:07 +0300

    Method channel failure alternative - Bug fix 4

--- just_audio_windows/windows/main_thread_dispatcher.cpp
+++ just_audio_windows/windows/main_thread_dispatcher.cpp
@@ -10,24 +10,21 @@ bool MainThreadDispatcher::Initialize() {
         return true;  // Already initialized
     }
 
-    DispatcherQueueOptions options = {
-            sizeof(DispatcherQueueOptions),
-            DQTYPE_THREAD_CURRENT,
-            DQTAT_COM_STA
-    };
-
-    // Use put_void() to get a raw pointer compatible with CreateDispatcherQueueController
-    HRESULT hr = CreateDispatcherQueueController(options, reinterpret_cast<PDISPATCHERQUEUECONTROLLER*>(controller_.put_void()));
-    if (SUCCEEDED(hr)) {
-        dispatcher_queue_ = controller_->DispatcherQueue();
-        return true;
-    } else {
+    // Create the DispatcherQueueController
+    DispatcherQueueOptions options = { sizeof(DispatcherQueueOptions), DQTYPE_THREAD_CURRENT, DQTAT_COM_STA };
+    HRESULT hr = CreateDispatcherQueueController(options, controller_.put_void());
+    if (FAILED(hr)) {
         return false;  // Initialization failed
     }
+
+    // Get the DispatcherQueue from the controller
+    dispatcher_queue_ = controller_->DispatcherQueue();
+    return true;
 }
 
 void MainThreadDispatcher::RunOnMainThread(std::function<void()> func) {
     if (dispatcher_queue_) {
+        // Try to enqueue the task to run on the main thread
         dispatcher_queue_.TryEnqueue([func]() { func(); });
     }
 }
