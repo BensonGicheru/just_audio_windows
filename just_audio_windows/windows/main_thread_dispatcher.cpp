31a142f0bdf5c758742f9bf42dbf743465157648
BenGicheru <bensgich@gmail.com>
2024-11-13 20:24:52 +0300

    Bug fix 2

--- just_audio_windows/windows/main_thread_dispatcher.cpp
+++ just_audio_windows/windows/main_thread_dispatcher.cpp
@@ -1,38 +1,35 @@
 #include "include/just_audio_windows/main_thread_dispatcher.h"
 
 // Singleton instance
+MainThreadDispatcher::MainThreadDispatcher() = default;
+
 MainThreadDispatcher& MainThreadDispatcher::Instance() {
     static MainThreadDispatcher instance;
     return instance;
 }
 
-// Initialize the dispatcher
 bool MainThreadDispatcher::Initialize() {
-    if (dispatcher_queue_ != nullptr) {
-        return true;  // Already initialized
+    if (controller_ != nullptr) {
+        return true; // Already initialized
     }
 
-    // Create the DispatcherQueueController
-    DispatcherQueueOptions options = { sizeof(DispatcherQueueOptions), DQTYPE_THREAD_CURRENT, DQTAT_COM_STA };
+    // Create a DispatcherQueueController instance
+    DispatcherQueueOptions options{};
+//    HRESULT hr = CreateDispatcherQueueController(options, controller_.put());
     HRESULT hr = CreateDispatcherQueueController(options, reinterpret_cast<PDISPATCHERQUEUECONTROLLER*>(controller_.put()));
     if (FAILED(hr)) {
-        return false;  // Initialization failed
+        return false; // Failed to create dispatcher controller
     }
 
     // Get the DispatcherQueue from the controller
     dispatcher_queue_ = controller_->DispatcherQueue();
-    return true;
+
+    return true; // Successfully initialized
 }
 
-// Run a function on the main thread
 void MainThreadDispatcher::RunOnMainThread(std::function<void()> func) {
-    if (dispatcher_queue_) {
-        // Enqueue the task to run on the main thread
-        dispatcher_queue_.TryEnqueue([func]() { func(); });
+    // Make sure the dispatcher queue is initialized
+    if (dispatcher_queue_ != nullptr) {
+        dispatcher_queue_.TryEnqueue(std::move(func));
     }
-}
-
-// Constructor to initialize the dispatcher on object creation
-MainThreadDispatcher::MainThreadDispatcher() {
-    Initialize();
 }
