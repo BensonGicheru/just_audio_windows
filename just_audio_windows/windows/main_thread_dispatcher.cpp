65f73da2f508a4c4fb273a02468966b76ba57318
BenGicheru <bensgich@gmail.com>
2024-11-14 10:08:40 +0300

    Method channel failure alternative fix - Bug fix

--- just_audio_windows/windows/main_thread_dispatcher.cpp
+++ just_audio_windows/windows/main_thread_dispatcher.cpp
@@ -27,15 +27,29 @@ bool MainThreadDispatcher::Initialize() {
     };
 
     // Use the DispatcherQueueController directly without casting to IDispatcherQueueController
-    winrt::Windows::System::DispatcherQueueController temp_controller{ nullptr };
-    HRESULT hr = CreateDispatcherQueueController(options, reinterpret_cast<PDISPATCHERQUEUECONTROLLER*>(winrt::put_abi(temp_controller)));
+//    winrt::Windows::System::DispatcherQueueController temp_controller{ nullptr };
+//    HRESULT hr = CreateDispatcherQueueController(options, reinterpret_cast<PDISPATCHERQUEUECONTROLLER*>(winrt::put_abi(temp_controller)));
+//
+//    if (FAILED(hr)) {
+//        return false; // Failed to create dispatcher controller
+//    }
+//
+//    // Assign the controller
+//    controller_ = temp_controller;
+
+    // Creating a temp controller and passing it using put()
+    winrt::com_ptr<winrt::Windows::System::IDispatcherQueueController> temp_controller;
+    HRESULT hr = CreateDispatcherQueueController(options, temp_controller.put());
 
     if (FAILED(hr)) {
+        // Log the HRESULT error for debugging
+        std::wcout << L"Failed to create DispatcherQueueController. HRESULT: " << std::hex << hr << std::endl;
         return false; // Failed to create dispatcher controller
     }
 
-    // Assign the controller
-    controller_ = temp_controller;
+    // Since controller_ is of type `winrt::Windows::System::DispatcherQueueController`
+    // we can safely assign `temp_controller` (which is a `com_ptr<IDispatcherQueueController>`) to it
+    controller_ = temp_controller.as<winrt::Windows::System::DispatcherQueueController>();
 
     // Retrieve and assign the dispatcher queue
     dispatcher_queue_ = controller_.DispatcherQueue();
