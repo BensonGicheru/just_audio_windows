74e323833991568ae01607526dc9442480e0543e
BenGicheru <bensgich@gmail.com>
2024-11-14 19:29:20 +0300

    Ensure MainThreadDispatcher is initialized on main thread

--- just_audio_windows/windows/main_thread_dispatcher.cpp
+++ just_audio_windows/windows/main_thread_dispatcher.cpp
@@ -17,37 +17,43 @@ MainThreadDispatcher& MainThreadDispatcher::Instance() {
 
 bool MainThreadDispatcher::Initialize() {
     std::wcout << L"[just_audio_windows]: MainThreadDispatcher::Initialize() called" << std::endl;
+
     if (controller_ != nullptr) {
         std::wcout << L"[just_audio_windows]: controller_ not null during MainThreadDispatcher initialize" << std::endl;
         return true; // Already initialized
     }
 
-    bool init_success = false;
+    // Ensure COM is initialized (needed for DispatcherQueue)
+    HRESULT hr_com_init = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+    if (FAILED(hr_com_init)) {
+        std::wcout << L"[just_audio_windows]: COM initialization failed, HRESULT: " << std::hex << hr_com_init << std::endl;
+        return false; // If COM initialization fails, return early
+    }
 
-    // Run on a background thread
-    std::thread dispatcher_init_thread([&]() {
-        DispatcherQueueOptions options{
-                sizeof(DispatcherQueueOptions),
-                DQTYPE_THREAD_CURRENT,
-                DQTAT_COM_NONE
-        };
+    DispatcherQueueOptions options{
+            sizeof(DispatcherQueueOptions),
+            DQTYPE_THREAD_CURRENT,  // This associates the current thread with the DispatcherQueue
+            DQTAT_COM_NONE
+    };
 
-        winrt::Windows::System::DispatcherQueueController temp_controller{ nullptr };
-        HRESULT hr = CreateDispatcherQueueController(options, reinterpret_cast<PDISPATCHERQUEUECONTROLLER*>(winrt::put_abi(temp_controller)));
+    winrt::Windows::System::DispatcherQueueController temp_controller{ nullptr };
+    hr = CreateDispatcherQueueController(options, reinterpret_cast<PDISPATCHERQUEUECONTROLLER*>(winrt::put_abi(temp_controller)));
 
-        if (FAILED(hr)) {
-            std::wcout << L"[just_audio_windows]: Failed to create DispatcherQueueController. HRESULT: " << std::hex << hr << std::endl;
-        } else {
-            std::wcout << L"[just_audio_windows]: [just_audio_windows]: Successfully created DispatcherQueueController on background thread" << std::endl;
-            controller_ = temp_controller;
-            dispatcher_queue_ = controller_.DispatcherQueue();
-            init_success = dispatcher_queue_ != nullptr;
-        }
-    });
+    if (FAILED(hr)) {
+        std::wcout << L"[just_audio_windows]: Failed to create DispatcherQueueController. HRESULT: " << std::hex << hr << std::endl;
+        CoUninitialize();  // Uninitialize COM
+        return false; // Failed to create dispatcher controller
+    } else {
+        std::wcout << L"[just_audio_windows]: Successfully created DispatcherQueueController" << std::endl;
+    }
 
-    dispatcher_init_thread.join(); // Wait for the thread to complete
+    // Assign the controller and dispatcher queue
+    controller_ = temp_controller;
+    dispatcher_queue_ = controller_.DispatcherQueue();
 
-    return init_success;
+    CoUninitialize();  // Uninitialize COM after use
+
+    return dispatcher_queue_ != nullptr;
 }
 
 void MainThreadDispatcher::RunOnMainThread(std::function<void()> func) {
